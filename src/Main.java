36f1a42323e3e8901f96cf5d668fa8df12c9b06d
med-salah-ben <med.salah.ben.amou@gmail.com>
2023-12-07 22:02:49 +0100

    Floating numbers

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        float number = 3.14f;
+        double number1 = 2.0;
+
+        float floatMaxValue = Float.MAX_VALUE;
+        float floatMinValue = Float.MIN_VALUE;
+        System.out.println("Float max Value = "+ floatMaxValue);
+        System.out.println("Float min Value = "+ floatMinValue);
+
+        double doubleMaxValue = Double.MAX_VALUE;
+        double doubleMinValue = Double.MIN_VALUE;
+        System.out.println("Double Max Value = " + doubleMaxValue);
+        System.out.println("Double Min Value = " + doubleMinValue);
+    }
+}
