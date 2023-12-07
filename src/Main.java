94bdd491ecd1d5691b34ba989ff895a96e208ace
med-salah-ben <med.salah.ben.amou@gmail.com>
2023-12-07 22:54:54 +0100

    Shortcut Of Arithmetic Operations

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,29 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        double num1 = 2 ; //percesion bigger than float
-        float num2 = 10;
-        System.out.println(num1 +  num2);
-        System.out.println(num1 +  num2 * 3);
-        System.out.println(num1 +  (num2 * 3));
-        System.out.println(num1 -  num2);
-        System.out.println(num1 *  num2);
-        System.out.println(num2 /  num1);
-        System.out.println(15 %  num1);
-        System.out.println(Math.pow(num1,2));
+        int num = 0 ;
+        num += 5 ;
+        System.out.println(num); // 5
+        num -= 3 ;
+        System.out.println(num); // 2
+        num *= 3;
+        System.out.println(num); // 6
+        num /= 2 ;
+        System.out.println(num); // 3
+        num %= 2 ;
+        System.out.println(num); // 1
+        num = 0 ;
+        System.out.println(num); // 0
+        num++;
+        System.out.println(num); // 1
+        ++num ;
+        System.out.println(num); // 2
+        num--;
+        System.out.println(num); // 1 // minus 1
+
+        System.out.println(num++); // 1 => Prefix incrementation :  add 1 after the print
+        System.out.println(num); // 2
+        System.out.println(++num); // 3 Postfix incrementation : add 1 and print
+
     }
 }
